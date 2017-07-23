60d84baf181df9ff47bfcdf64d0dc5bb31c0b487
anthony middleton <ammiddleton@email.arizona.edu>
2017-07-23 13:43:19 -0700

    controller.java 07/23/17

--- src/controller_view/Iteration1Controller.java
+++ src/controller_view/Iteration1Controller.java
@@ -1,8 +1,11 @@
 package controller_view;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Iteration1Controller extends Application {
@@ -10,7 +13,10 @@ public class Iteration1Controller extends Application {
   public static void main(String[] args) {
     launch(args);
   }
-  
+  private Button buttonSong1 = new Button("Select song 1");
+  private Button buttonSong2 = new Button("Select song 2");
+
+
  
   @Override
   public void start(Stage primaryStage) throws Exception {
@@ -18,6 +24,15 @@ public class Iteration1Controller extends Application {
     BorderPane all = new BorderPane();
     Scene scene = new Scene(all, 300, 230);
     primaryStage.setScene(scene);
+    
+	HBox hbox = new HBox();
+	hbox.setPadding(new Insets(15, 12, 15, 12));
+	hbox.setSpacing(10);
+	
+	hbox.getChildren().addAll(buttonSong1, buttonSong2);
+
+	all.setTop(hbox);
+
 
     // Don't forget to show the running application:
     primaryStage.show();
