55259353515f39e8245c513c635c33da57095431
svanhoesen <svanhoesen.email.arizona.edu>
2017-07-23 16:37:28 -0700

    Uploading some more files

--- src/demoMediaPlayer/MP3Player.java
+++ src/demoMediaPlayer/MP3Player.java
@@ -10,6 +10,7 @@ import java.awt.EventQueue;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -20,6 +21,9 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 public class MP3Player extends Thread {
 
@@ -41,4 +45,38 @@ public class MP3Player extends Thread {
 		play();
 	}
 
+	public void play() {
+		try {
+			File file = new File(fileName);
+			 URI uri = file.toURI();
+			 System.out.println(uri);
+			    Media media = new Media(uri.toString());
+			    MediaPlayer mediaPlayer = new MediaPlayer(media);
+			    mediaPlayer.setAutoPlay(true);
+			    mediaPlayer.play();
+			    
+			    mediaPlayer.setOnEndOfMedia(new EndOfSongHandler(null, null));
+
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+
+	private class EndOfSongHandler implements Runnable {
+
+		private EOSEvent endEvent;
+		private EOSListener endListener;
+
+		public EndOfSongHandler(EOSEvent endEvent, EOSListener endListener) {
+			this.endEvent = endEvent;
+			this.endListener = endListener;
+		}
+
+		@Override
+		public void run() {
+			endListener.songHasEnded(endEvent);
+		}
+	}
 }
