9561cc04ec109dc9c1f257c31531ced50eb7e228
anthony middleton <ammiddleton@email.arizona.edu>
2017-07-23 12:23:30 -0700

    Song.java 07/23/17

--- src/model/Song.java
+++ src/model/Song.java
@@ -1,16 +1,36 @@
 package model;
 
 public class Song {
-	private String title;
+	String title;
 	private int seconds;
 	private String artist;
 	private String fileName;
+	private int played;
+	private int maxPlay = 3;
 
-	public Song(String songTitle, int songLenght, String arhtur, String fileName) {
-		this.title = songTitle;
-		this.seconds = songLenght;
-		this.artist = arhtur;
+
+	public Song(String title, int seconds, String artist, String fileName) {
+		this.title = title;
+		this.seconds = seconds;
+		this.artist = artist;
 		this.fileName = fileName;
+		played = maxPlay;
+	}
+	
+	public String getSongTitle() {
+		return title;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public int gettime() {
+		return seconds;
+	}
+
+	public String getLocation() {
+		return fileName;
 	}
 
 	public void PlayMe() {
@@ -28,8 +48,10 @@ public class Song {
 	}
 
 	public boolean canBePlayedToday() {
-		// TODO Auto-generated method stub
-		return false;
+		if (played == 0){
+			return false;
+		}
+		return true;
 	}
 
 }
