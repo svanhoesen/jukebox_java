5f8d997453581c6ebdfc134a29a1d4974abb8af8
svanhoesen <svanhoesen.email.arizona.edu>
2017-08-02 20:58:03 -0700

    Uploading some more files

---
+++ src/model/AdminCollection.java
@@ -0,0 +1,20 @@
+package model;
+
+/*
+ * Author: Steffan Van Hoesen and Anthony Middleton
+ * 
+ * Class StudentCollection: This is the class student names. Contain all the information that a student should have.
+ */
+import java.io.Serializable;
+import java.util.HashMap;
+
+public class AdminCollection extends HashMap<String, Student> implements Serializable {
+
+	public AdminCollection() {
+			
+		}
+
+	public boolean validateAdmin(String userName, String password) {
+		return get(userName).getPassword().equals(password);
+	}
+}
---
+++ src/persistence/WindowClose.java
@@ -0,0 +1,89 @@
+package persistence;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.JOptionPane;
+
+import model.Song;
+import model.SongCollection;
+import model.Student;
+import model.JukeBox;
+
+public class WindowClose implements WindowListener{
+
+	private JukeBox box = new JukeBox();
+	private SongCollection songCollection = new SongCollection();
+	
+	@SuppressWarnings("unchecked")
+	public void readInputFile() {
+		try {
+			FileInputStream rawBytes = new FileInputStream("myPlayList");
+			ObjectInputStream inFile = new ObjectInputStream(rawBytes);
+			box.setTrackList((ArrayList<Song>) inFile.readObject());
+			box.setStudentList((HashMap<String, Student>) inFile.readObject());
+			songCollection.recover((SongCollection) inFile.readObject());
+			inFile.close();
+		} catch (Exception e) {
+		}
+
+	}
+	
+	@Override
+	public void windowOpened(WindowEvent e) {
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		int decision = JOptionPane.showConfirmDialog(null, "Save Current Stage?");
+		if (decision == JOptionPane.YES_OPTION) {
+			try {
+				FileOutputStream bytesToDisk = new FileOutputStream("myPlayList");
+				ObjectOutputStream outFile = new ObjectOutputStream(bytesToDisk);
+				// outFile understands the writeObject(Object o) message.
+				outFile.writeObject(box.getTrackList());
+				outFile.writeObject(box.getUserList());
+				outFile.writeObject(songCollection);
+				outFile.close(); // Always close the output file!
+				System.exit(0);
+			} catch (IOException ioe) {
+				System.out.println("Writing objects failed");
+			}
+
+		} else if (decision == JOptionPane.NO_OPTION)
+			System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+
+	}
+
+}
